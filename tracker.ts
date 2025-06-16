78008aa9ed2df7f76c920ff6757a45ba8ab935b3
eric-livezey <eric.livezey@gmail.com>
2025-06-16 12:59:22 -0400

    Updated tracker implementation

--- tracker.ts
+++ tracker.ts
@@ -1,25 +1,88 @@
 import { EventEmitter } from 'node:events';
 import { WebSocket } from 'ws';
-import { Client, Resource, ResourceIdentifier, ResourceMap, ResourceType, RouteResource, RouteType, VehicleResource } from "./api/mbta";
+import { Client, Resource, ResourceIdentifier, ResourceMap, ResourceType, RouteResource, RouteType, ShapeResource, VehicleResource } from "./api/mbta";
 
 /**
  * Types of routes which should be tracked.
  */
 const ROUTES_TYPES = [RouteType.LIGHT_RAIL, RouteType.HEAVY_RAIL];
 
-type Unionable<T> = T extends {} ? T : {};
-type APIResource<T extends Resource = Resource, U extends Record<string, any> = Unionable<T['attributes']>> = Pick<T, 'id'> & U;
-type APIIdentifier<T extends APIResource = APIResource> = Pick<T, 'id'>;
-type APIRoute = APIResource<RouteResource, RouteResource['attributes'] & { shapes: APIResourceMap['shape'][] }>;
-type APIVehicle = APIResource<VehicleResource, VehicleResource['attributes'] & { route_id?: string }>;
-interface APICustomResourceMap {
-    route: APIRoute;
-    vehicle: APIVehicle;
+const MAPPINGS = (<T extends { [P in ResourceType]?: (resource: ResourceMap[P], state: Partial<ResourceCache>) => any }>(value: T) => value)({
+    route: ({ id, attributes }, state) => {
+        // get related shapes
+        const shapes = [];
+        for (const { relationships } of state.trip?.values() || []) {
+            if (relationships?.route?.data?.id === id && state.shape && relationships.shape?.data) {
+                const shape = state.shape.get(relationships.shape.data.id);
+                if (shape) {
+                    const { id, attributes } = shape
+                    shapes.push({ id: id, polyline: attributes!.polyline });
+                }
+            }
+        }
+        return {
+            id,
+            shapes: shapes,
+            type: attributes!.type!,
+            textColor: attributes!.text_color,
+            sortOrder: attributes!.sort_order,
+            shortName: attributes!.short_name,
+            longName: attributes!.long_name,
+            fareClass: attributes!.fare_class,
+            directionNames: attributes!.direction_names,
+            directionDestinations: attributes!.direction_destinations,
+            description: attributes!.description,
+            color: attributes!.color
+        };
+    },
+    vehicle: ({ id, attributes, relationships }) => ({
+        id,
+        routeId: relationships!.route!.data!.id,
+        updatedAt: attributes!.updated_at!,
+        latitude: attributes!.latitude!,
+        longitude: attributes!.longitude!,
+        bearing: attributes!.bearing!,
+        speed: attributes!.speed,
+        revenueStatus: attributes!.revenue_status,
+        occupancyStatus: attributes!.occupancy_status,
+        label: attributes!.label,
+        directionId: attributes!.direction_id,
+        currentStopSequence: attributes!.current_stop_sequence,
+        currentStatus: attributes!.current_status,
+        carriages: attributes!.carriages?.map(carriage => ({
+            occupancyStatus: carriage.occupancy_status,
+            occupancyPercentage: carriage.occupancy_percentage,
+            label: carriage.label
+        }))
+    }),
+    stop: ({ id, attributes }, state) => ({
+        id,
+        routeIds: state.route, // TODO
+        name: attributes!.name!,
+        latitude: attributes!.latitude!,
+        longitude: attributes!.longitude!,
+        wheelchairBoarding: attributes!.wheelchair_boarding,
+        vehicleType: attributes!.vehicle_type,
+        platformName: attributes!.platform_name,
+        platformCode: attributes!.platform_code,
+        onStreet: attributes!.on_street,
+        municipality: attributes!.municipality,
+        locationType: attributes!.location_type,
+        description: attributes!.description,
+        atStreet: attributes!.at_street,
+        address: attributes!.address
+    })
+});
+type Mappings = typeof MAPPINGS;
+interface APIIdentifier {
+    id: string;
 }
 type APIResourceMap = {
-    [P in ResourceType]: P extends keyof APICustomResourceMap ? APICustomResourceMap[P] : APIResource<ResourceMap[P]>;
+    [P in keyof Mappings]: ReturnType<Mappings[P]>;
 };
-type Association<T extends ResourceType = ResourceType> = {
+type APIResourceType = keyof APIResourceMap;
+type APIResource = APIResourceMap[APIResourceType];
+type Association<T extends APIResourceType = APIResourceType> = {
     type: T;
     get: (cache: Partial<ResourceCache>, id?: string) => APIResourceMap[T][];
 }
@@ -34,13 +97,13 @@ interface StreamEventMap<T extends APIResource = APIResource> {
     remove: APIIdentifier;
 }
 type EventType = keyof StreamEventMap;
-type PayloadMap<T extends ResourceType = ResourceType> = {
+type PayloadMap<T extends APIResourceType = APIResourceType> = {
     [P in EventType]: {
         type: T;
         data: StreamEventMap<APIResourceMap[T]>[P];
     };
 };
-type EventMap<T extends ResourceType = ResourceType> = {
+type EventMap<T extends APIResourceType = APIResourceType> = {
     [P in EventType]: [PayloadMap<T>[P]];
 };
 class TrackerCache extends EventEmitter<EventMap> {
@@ -71,10 +134,10 @@ class TrackerCache extends EventEmitter<EventMap> {
         return this.#cache[type] as Map<string, ResourceMap[T]>;
     }
 
-    #getPayload(type: ResourceType, id?: string): { type: ResourceType, data: APIResource[] } | undefined {
+    #getPayload(type: ResourceType, id?: string): { type: APIResourceType, data: APIResource[] } | undefined {
         if (this.#isTracked(type)) {
-            const association = this.#associations[type] as Association;
-            return { type, data: association.get(this.#cache, id) }
+            const association = this.#associations[type]!;
+            return { type: association.type, data: association.get(this.#cache, id) }
         }
     }
 
@@ -136,18 +199,23 @@ class TrackerCache extends EventEmitter<EventMap> {
         }
     }
 
-    setAssociation<T extends ResourceType, U extends ResourceType>(sourceType: T, resultType: U, get: Association<U>['get'] = (cache, id) => {
-        const resources = id ? cache[sourceType]?.get(id) || [] : Array.from(cache[sourceType]?.values() || []);
-        return (Array.isArray(resources) ? resources : [resources]).map(resource => ({ ...resource.attributes, id: resource.id } as APIResourceMap[U]));
-    }) {
+    track<T extends APIResourceType>(type: T) {
+        this.associate(type, type, (cache, id) => {
+            const fn = MAPPINGS[type] as (resource: ResourceMap[T], state: Partial<ResourceCache>) => any;
+            const resources = id ? cache[type]?.get(id) || [] : Array.from(cache[type]?.values() || []);
+            return (Array.isArray(resources) ? resources : [resources]).map(resource => fn(resource, cache));
+        });
+    }
+
+    associate<T extends ResourceType, U extends APIResourceType>(sourceType: T, resultType: U, get: Association<U>['get']) {
         this.#associations[sourceType] = { type: resultType, get };
     }
 
-    setRelationship(sourceType: ResourceType, resultType: ResourceType, through: string) {
+    relate(sourceType: ResourceType, resultType: APIResourceType, through: string) {
         if (resultType in this.#associations) {
             const association = this.#associations[resultType] as Association;
             const parts = through.split('.');
-            this.setAssociation(sourceType, resultType, (cache, id) => {
+            this.associate(sourceType, resultType, (cache, id) => {
                 const resources: Resource | Resource[] = (id ? cache[sourceType]?.get(id) || [] : Array.from<ResourceMap[keyof ResourceMap]>(cache[sourceType]?.values() || []));
                 return (Array.isArray(resources) ? resources : [resources]).flatMap(resource => {
                     // loop through each relationship to create the list of resources
@@ -236,28 +304,13 @@ class Tracker {
          * Maybe also the following data at significantly less frequent intervals:
          * - Routes, Shapes, Stops, Alerts, Lines
          */
-        this.#cache.setAssociation('route', 'route', (cache, id) => {
-            const resources = id ? cache.route?.get(id) || [] : Array.from(cache.route?.values() || []);
-            return (Array.isArray(resources) ? resources : [resources]).map(resource => {
-                // get included shapes assocated with the route
-                const trips = Array.from(cache.trip?.values() || []).filter(value => value.relationships?.route?.data?.id === resource.id);
-                const shapes = [];
-                for (const resource of cache.shape?.values() || []) {
-                    if (trips?.some(trip => trip.relationships?.shape?.data?.id === resource.id)) {
-                        shapes.push({ id: resource.id, ...resource.attributes });
-                    }
-                }
-                return { ...resource.attributes, id: resource.id, shapes };
-            })
-        });
-        this.#cache.setAssociation('vehicle', 'vehicle', (cache, id) => {
-            const resources = id ? cache.vehicle?.get(id) || [] : Array.from(cache.vehicle?.values() || []);
-            return (Array.isArray(resources) ? resources : [resources]).map(resource => ({ ...resource.attributes, id: resource.id, route_id: resource.relationships?.route?.data?.id }));
-        });
-        this.#cache.setRelationship('route_pattern', 'route', 'route');
-        this.#cache.setRelationship('trip', 'route', 'route_pattern.route');
-        const routeTypeFilter = ROUTES_TYPES.join(',')
-        const routes = this.client.streamRoutes({ 'filter[type]': routeTypeFilter, include: 'route_patterns.representative_trip.shape' });
+        this.#cache.track('route');
+        this.#cache.track('vehicle');
+        this.#cache.track('stop');
+        this.#cache.relate('route_pattern', 'route', 'route');
+        this.#cache.relate('trip', 'route', 'route_pattern.route');
+        const routeTypeFilter = ROUTES_TYPES.join(',');
+        const routes = this.client.streamRoutes({ 'filter[type]': routeTypeFilter, include: 'route_patterns.representative_trip.shape,route_patterns.representative_trip.stops' });
         this.#cache.bind(routes);
         const vehicles = this.client.streamVehicles({ 'filter[route_type]': routeTypeFilter });
         this.#cache.bind(vehicles);
