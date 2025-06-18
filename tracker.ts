81ac97f9630a567ac28b513473ac8aa2861a1603
eric-livezey <eric.livezey@gmail.com>
2025-06-18 11:34:12 -0400

    Websocket filters

--- tracker.ts
+++ tracker.ts
@@ -14,7 +14,7 @@ import { Client, Document, Resource, ResourceIdentifier, ResourceMap, ResourceTy
  */
 const ROUTES_TYPES = [RouteType.LIGHT_RAIL, RouteType.HEAVY_RAIL];
 
-const MAPPERS = (<T extends { [P in APIResourceType]: (resource: ResourceMap[P], state: Partial<ResourceCache>) => APIIdentifier }>(value: T) => value)({
+const MAPPERS = (<T extends { [P in APIResourceType]: (resource: ResourceMap[P], state: Partial<ResourceCache>) => EventResource<APIResource> }>(value: T) => value)({
     route: mapRoute,
     vehicle: mapVehicle,
     stop: mapStop
@@ -34,35 +34,55 @@ type IAPIResourceMap = {
     [P in APIResourceType]: APIIdentifier;
 };
 interface APIResourceMap extends IAPIResourceMap {
-    route: ReturnType<typeof mapRoute>;
-    vehicle: ReturnType<typeof mapVehicle>;
-    stop: ReturnType<typeof mapStop>;
+    route: ReturnType<typeof mapRoute>['resource'];
+    vehicle: ReturnType<typeof mapVehicle>['resource'];
+    stop: ReturnType<typeof mapStop>['resource'];
 }
 type APIResource = APIResourceMap[APIResourceType];
 type ResourceCache = {
     [P in ResourceType]: Map<string, ResourceMap[P]>;
 };
-type AssociateFunction<T extends APIResourceType = APIResourceType> = (state: Partial<ResourceCache>, id?: string) => APIResourceMap[T][]
+type AssociateFunction<T extends APIResourceType = APIResourceType> = (state: Partial<ResourceCache>, id?: string) => EventResource<APIResourceMap[T]>[]
 /**
  * A function which requests a document of resources.
  */
 type RequestFunction = () => Promise<Document>;
 type EventType = 'reset' | 'add' | 'update' | 'remove';
-interface StreamEventMap<T extends APIResourceType = APIResourceType> extends Record<EventType, APIIdentifier[] | APIIdentifier> {
-    reset: APIResourceMap[T][];
-    add: APIResourceMap[T];
-    update: APIResourceMap[T];
-    remove: APIIdentifier;
+interface EventResource<T extends APIIdentifier = APIIdentifier> {
+    resource: T;
+    routes: RouteResource[];
 }
-type PayloadMap<T extends APIResourceType = APIResourceType> = {
+interface StreamEventMap<T extends APIResourceType = APIResourceType> extends Record<EventType, EventResource[] | EventResource> {
+    reset: EventResource<APIResourceMap[T]>[];
+    add: EventResource<APIResourceMap[T]>;
+    update: EventResource<APIResourceMap[T]>;
+    remove: EventResource;
+}
+type EventPayloadMap<T extends APIResourceType = APIResourceType> = {
     [P in EventType]: {
         type: T;
         data: StreamEventMap<T>[P];
     };
 };
+type PayloadMap<T extends APIResourceType = APIResourceType> = {
+    [P in keyof EventPayloadMap]: {
+        type: T;
+        data: P extends 'reset' ? APIResourceMap[T][] : StreamEventMap<T>[Exclude<P, 'reset'>]['resource'];
+    };
+}
 type EventMap<T extends APIResourceType = APIResourceType> = {
-    [P in EventType]: [PayloadMap<T>[P]];
+    [P in EventType]: [EventPayloadMap<T>[P]];
 };
+interface EventFilter {
+    types: Set<APIResourceType>;
+    routeIds: Set<string> | null;
+    routeTypes: Set<RouteType> | null;
+}
+interface WebsocketPayload {
+    types: APIResourceType[]
+    routeIds?: string[];
+    routeTypes?: RouteType[];
+}
 
 /*
  * -------------
@@ -70,7 +90,8 @@ type EventMap<T extends APIResourceType = APIResourceType> = {
  * -------------
  */
 
-function mapRoute({ id, attributes }: RouteResource, state: Partial<ResourceCache>) {
+function mapRoute(route: RouteResource, state: Partial<ResourceCache>) {
+    const { id, attributes } = route;
     // get related shapes
     const shapes = [];
     for (const { relationships } of state.trip?.values() || []) {
@@ -82,69 +103,90 @@ function mapRoute({ id, attributes }: RouteResource, state: Partial<ResourceCach
         }
     }
     return {
-        id,
-        shapes: shapes,
-        type: attributes!.type!,
-        textColor: attributes!.text_color,
-        sortOrder: attributes!.sort_order,
-        shortName: attributes!.short_name,
-        longName: attributes!.long_name,
-        fareClass: attributes!.fare_class,
-        directionNames: attributes!.direction_names,
-        directionDestinations: attributes!.direction_destinations,
-        description: attributes!.description,
-        color: attributes!.color
+        resource: {
+            id,
+            shapes: shapes,
+            type: attributes!.type!,
+            textColor: attributes!.text_color,
+            sortOrder: attributes!.sort_order,
+            shortName: attributes!.short_name,
+            longName: attributes!.long_name,
+            fareClass: attributes!.fare_class,
+            directionNames: attributes!.direction_names,
+            directionDestinations: attributes!.direction_destinations,
+            description: attributes!.description,
+            color: attributes!.color
+        },
+        routes: [route]
     };
 }
 
-function mapVehicle({ id, attributes, relationships }: VehicleResource) {
+function mapVehicle({ id, attributes, relationships }: VehicleResource, state: Partial<ResourceCache>) {
     return {
-        id,
-        routeId: relationships!.route!.data!.id,
-        updatedAt: attributes!.updated_at!,
-        latitude: attributes!.latitude!,
-        longitude: attributes!.longitude!,
-        bearing: attributes!.bearing!,
-        speed: attributes!.speed,
-        revenueStatus: attributes!.revenue_status,
-        occupancyStatus: attributes!.occupancy_status,
-        label: attributes!.label,
-        directionId: attributes!.direction_id,
-        currentStopSequence: attributes!.current_stop_sequence,
-        currentStatus: attributes!.current_status,
-        carriages: attributes!.carriages?.map(carriage => ({
-            occupancyStatus: carriage.occupancy_status,
-            occupancyPercentage: carriage.occupancy_percentage,
-            label: carriage.label
-        }))
+        resource: {
+            id,
+            routeId: relationships!.route!.data!.id,
+            updatedAt: attributes!.updated_at!,
+            latitude: attributes!.latitude!,
+            longitude: attributes!.longitude!,
+            bearing: attributes!.bearing!,
+            speed: attributes!.speed,
+            revenueStatus: attributes!.revenue_status,
+            occupancyStatus: attributes!.occupancy_status,
+            label: attributes!.label,
+            directionId: attributes!.direction_id,
+            currentStopSequence: attributes!.current_stop_sequence,
+            currentStatus: attributes!.current_status,
+            carriages: attributes!.carriages?.map(carriage => ({
+                occupancyStatus: carriage.occupancy_status,
+                occupancyPercentage: carriage.occupancy_percentage,
+                label: carriage.label
+            }))
+        },
+        routes: [state.route!.get(relationships!.route!.data!.id)!]
     };
 }
 
 function mapStop({ id, attributes }: StopResource, state: Partial<ResourceCache>) {
     const routeIds = new Set<string>();
+    const routes = [];
     for (const trip of state?.trip?.values() || []) {
         const routeId = trip.relationships?.route?.data?.id;
-        if (routeId && trip.relationships?.stops?.data?.some(({ id: stopId }) => stopId === id)) {
+        if (routeId && trip.relationships?.stops?.data?.some(({ id: stopId }) => stopId === id) && !routeIds.has(routeId)) {
             routeIds.add(routeId);
+            routes.push(state.route!.get(routeId)!);
         }
     }
     return {
-        id,
-        routeIds: Array.from(routeIds),
-        name: attributes!.name!,
-        latitude: attributes!.latitude!,
-        longitude: attributes!.longitude!,
-        wheelchairBoarding: attributes!.wheelchair_boarding,
-        vehicleType: attributes!.vehicle_type,
-        platformName: attributes!.platform_name,
-        platformCode: attributes!.platform_code,
-        onStreet: attributes!.on_street,
-        municipality: attributes!.municipality,
-        locationType: attributes!.location_type,
-        description: attributes!.description,
-        atStreet: attributes!.at_street,
-        address: attributes!.address
+        resource: {
+            id,
+            routeIds: Array.from(routeIds),
+            name: attributes!.name!,
+            latitude: attributes!.latitude!,
+            longitude: attributes!.longitude!,
+            wheelchairBoarding: attributes!.wheelchair_boarding,
+            vehicleType: attributes!.vehicle_type,
+            platformName: attributes!.platform_name,
+            platformCode: attributes!.platform_code,
+            onStreet: attributes!.on_street,
+            municipality: attributes!.municipality,
+            locationType: attributes!.location_type,
+            description: attributes!.description,
+            atStreet: attributes!.at_street,
+            address: attributes!.address
+        },
+        routes: routes
     }
+}
+
+function matchesFilters(type: APIResourceType, routes: RouteResource[], filter: EventFilter) {
+    if (filter.types && !filter.types.has(type)) {
+        return false;
+    }
+    if (routes.some(route => filter.routeIds && !filter.routeIds.has(route.id) || route.attributes?.type == null || filter.routeTypes && !filter.routeTypes.has(route.attributes.type))) {
+        return false;
+    }
+    return true;
 }
 
 /**
@@ -189,10 +231,13 @@ class TrackerCache extends EventEmitter<EventMap> {
     #requesters: RequestFunction[];
 
     get routes() {
-        return (this.#getPayload('route')?.data || []) as APIResourceMap['route'][];
+        return (this.#getPayload('route')?.data || []) as EventResource<APIResourceMap['route']>[];
     }
     get vehicles() {
-        return (this.#getPayload('vehicle')?.data || []) as APIResourceMap['vehicle'][];
+        return (this.#getPayload('vehicle')?.data || []) as EventResource<APIResourceMap['vehicle']>[];
+    }
+    get stops() {
+        return (this.#getPayload('stop')?.data || []) as EventResource<APIResourceMap['stop']>[];
     }
 
     constructor() {
@@ -232,7 +277,7 @@ class TrackerCache extends EventEmitter<EventMap> {
         return this.#state[type] as Map<string, ResourceMap[T]>;
     }
 
-    #getPayload(type: ResourceType, id?: string): { type: APIResourceType, data: APIResource[] } | undefined {
+    #getPayload(type: ResourceType, id?: string): { type: APIResourceType, data: EventResource<APIResource>[] } | undefined {
         if (this.#isTracked(type)) {
             const fn = this.#associations[type]!;
             return { type, data: fn(this.#state, id) }
@@ -292,7 +337,7 @@ class TrackerCache extends EventEmitter<EventMap> {
         const payload = this.#getPayload(type, id);
         if (payload) {
             for (const data of payload.data) {
-                this.emit('remove', { type: payload.type, data: { id: data.id } });
+                this.emit('remove', { type: payload.type, data: { resource: { id: data.resource.id }, routes: data.routes } });
             }
         }
     }
@@ -316,17 +361,16 @@ class TrackerCache extends EventEmitter<EventMap> {
         es.addEventListener('remove', (event) => this.remove(JSON.parse(event.data)));
     }
 
-    addRequester(fn: RequestFunction) {
+    async addRequester(fn: RequestFunction) {
         this.#requesters.push(fn);
         // send initial reset event
-        fn().then((document) => {
-            if ('data' in document) {
-                const data = Array.isArray(document.data) ? document.data : [document.data];
-                const included = document.included || [];
-                const resources = [...data, ...included];
-                this.reset(resources);
-            }
-        })
+        const document = await fn();
+        if ('data' in document) {
+            const data = Array.isArray(document.data) ? document.data : [document.data];
+            const included = document.included || [];
+            const resources = [...data, ...included];
+            this.reset(resources);
+        }
     }
 }
 /**
@@ -345,51 +389,85 @@ class Tracker {
     }
 
     attach(ws: WebSocket) {
-        const listener = <T extends EventType>(event: T, { type, data }: PayloadMap[T]) => {
+        const filters: EventFilter = {
+            types: new Set(),
+            routeIds: new Set(),
+            routeTypes: new Set()
+        };
+        function forward<T extends EventType>(event: T, { type, data }: PayloadMap[T]) {
             const payload = {
                 type: `${type.toUpperCase()}_${event.toUpperCase()}`,
                 data: data
             };
             ws.send(JSON.stringify(payload));
         }
-        const resetListener = listener.bind(this, 'reset');
-        const addListener = listener.bind(this, 'add');
-        const updateListener = listener.bind(this, 'update');
-        const removeListener = listener.bind(this, 'remove');
-        // sent initial reset events to sync data
-        resetListener({ type: 'route', data: this.#cache.routes });
-        resetListener({ type: 'vehicle', data: this.#cache.vehicles });
+        function reset({ type, data }: EventPayloadMap['reset']) {
+            data = data.filter(resource => matchesFilters(type, resource.routes, filters));
+            if (data.length > 0) {
+                forward('reset', { type, data: data.map(er => er.resource) });
+            }
+        }
+        function add({ type, data }: EventPayloadMap['add']) {
+            if (matchesFilters(type, data.routes, filters)) {
+                forward('add', { type, data: data.resource });
+            }
+        }
+        function update({ type, data }: EventPayloadMap['update']) {
+            if (matchesFilters(type, data.routes, filters)) {
+                forward('update', { type, data: data.resource });
+            }
+        }
+        function remove({ type, data }: EventPayloadMap['remove']) {
+            if (matchesFilters(type, data.routes, filters)) {
+                forward('remove', { type, data: data.resource });
+            }
+        }
         ws.once("close", () => {
-            this.#cache.removeListener('reset', resetListener);
-            this.#cache.removeListener('add', addListener);
-            this.#cache.removeListener('update', updateListener);
-            this.#cache.removeListener('remove', removeListener);
+            this.#cache.removeListener('reset', reset);
+            this.#cache.removeListener('add', add);
+            this.#cache.removeListener('update', update);
+            this.#cache.removeListener('remove', remove);
         });
-        this.#cache.on('reset', resetListener);
-        this.#cache.on('add', addListener);
-        this.#cache.on('update', updateListener);
-        this.#cache.on('remove', removeListener);
+        this.#cache.on('reset', reset);
+        this.#cache.on('add', add);
+        this.#cache.on('update', update);
+        this.#cache.on('remove', remove);
+        ws.addEventListener('message', (event) => {
+            const json = event.data.toString();
+            try {
+                const data: WebsocketPayload = JSON.parse(json);
+                filters.types = new Set(data.types);
+                filters.routeIds = data.routeIds ? new Set(data.routeIds) : null;
+                filters.routeTypes = data.routeTypes ? new Set(data.routeTypes) : null;
+                // sent initial reset events to sync data
+                reset({ type: 'route', data: this.#cache.routes });
+                reset({ type: 'vehicle', data: this.#cache.vehicles });
+                reset({ type: 'stop', data: this.#cache.stops });
+            } catch (e) {
+                console.error(e);
+            }
+        });
     }
 
     /**
      * Initializes tracking.
      */
     async track() {
-        // TODO: Collect API data at regular intervals.
         /*
          * Collect the following data:
          * - Vehicles - For position/speed/direction & any other data we might want to show
          * - Schedules - For schedules times
          * - Predictions - For predicted times
+         * - Alerts - For alerts
          * Maybe also the following data at significantly less frequent intervals:
-         * - Routes, Shapes, Stops, Alerts, Lines
+         * - Routes, Shapes, Stops, Lines
          */
         this.#cache.track('route');
         this.#cache.track('vehicle');
         this.#cache.track('stop');
         const routeTypeFilter = ROUTES_TYPES.join(',');
         const routesfn = this.client.getRoutes.bind(this.client, { 'filter[type]': routeTypeFilter, include: 'route_patterns.representative_trip.shape,route_patterns.representative_trip.stops' });
-        this.#cache.addRequester(routesfn);
+        await this.#cache.addRequester(routesfn);
         const vehicles = this.client.streamVehicles({ 'filter[route_type]': routeTypeFilter });
         this.#cache.bind(vehicles);
     }
